81d98bbb558e2c2c1faad456da1a09768fa0a632
Nicholas Stees <nstees@gmail.com>
2019-04-30 07:41:52 -0400

    inital commit

---
+++ src/main.js
@@ -0,0 +1,19 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+
+import DefaultLayout from "~/layouts/Default.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCode, faLink } from "@fortawesome/free-solid-svg-icons";
+
+// Globaly add these icons for use
+library.add(faCode, faLink);
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+import "~/assets/global.scss";
+
+export default function(Vue, { router, head, isClient }) {
+  // Set default layout as a global component
+  Vue.component("Layout", DefaultLayout);
+  Vue.component("font-awesome-icon", FontAwesomeIcon);
+}
